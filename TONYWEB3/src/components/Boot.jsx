48e53e7aa27a285f2149846b0d92dd1e68b5cf02
BlairArkham <77673662+BlairArkham@users.noreply.github.com>
2021-07-30 15:50:09 +0300

    Add files via upload

---
+++ TONYWEB3/src/components/Boot.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Boot() {
+  return (
+    <div className="note">
+      <h1>CyberSecurity Bootcamp</h1>
+      <p>This bootcamp was very productive we generated API keys </p>
+    </div>
+  );
+}
+export default Boot;
